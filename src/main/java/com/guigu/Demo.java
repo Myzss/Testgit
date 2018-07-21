5d6e9850afd941a703c0bb89e5e426c6568365b6
tom_glb <tom_email_glb@qq.com>
2018-07-21 14:31:54 +0800

    commit gidDemo  add syso

---
+++ src/main/java/com/guigu/Demo.java
@@ -0,0 +1,8 @@
+package com.guigu;
+
+public class Demo {
+	public static void main(String[] args) {
+		System.out.println("demo first");
+	}
+
+}
---
+++ src/test/java/com/guigu/test/TestDemo.java
@@ -0,0 +1,6 @@
+package com.guigu.test;
+
+public class TestDemo {
+	public static void main(String[] args) {
+	}
+}
