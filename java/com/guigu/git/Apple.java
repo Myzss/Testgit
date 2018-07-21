79b890e901ec8c0d02d649e60b4cdb742b1d0684
tom_glb <tom_email_glb@qq.com>
2018-07-21 11:32:49 +0800

    dd

---
+++ java/com/guigu/git/Apple.java
@@ -0,0 +1,10 @@
+package com.guigu.git;
+
+/**
+ * git2
+ * */
+public class Apple {
+	public static void main(String[] args) {
+		System.out.println("git2");
+	}
+}
--- src/main/java/com/guigu/git/Apple.java
+++
@@ -1,5 +0,0 @@
-package com.guigu.git;
-
-public class Apple {
-
-}
